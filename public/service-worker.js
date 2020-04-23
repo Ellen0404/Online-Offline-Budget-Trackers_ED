1ed94494d902161de1a3d39e73a9b2434cecab54
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-04-23 15:32:09 -0600

    manifest and server-workers

---
+++ public/service-worker.js
@@ -0,0 +1,86 @@
+const urlsToCache = [
+    "/",
+    // "/indexDb.js",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css"
+
+];
+
+const CACHE_NAME = "static-files-v1";
+const DATA_CACHE_NAME = "data-cache-v2";
+
+// install
+self.addEventListener("install", function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+// activate
+// self.addEventListener("activate", function(evt) {
+//   evt.waitUntil(
+//     caches.keys().then(keyList => {
+//       return Promise.all(
+//         keyList.map(key => {
+//           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+//             console.log("Removing old cache data", key);
+//             return caches.delete(key);
+//           }
+//         })
+//       );
+//     })
+//   );
+
+//   self.clients.claim();
+// });
+
+self.addEventListener("fetch", function (event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(event.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match("/");
+                }
+            });
+        })
+        // caches.open(CACHE_NAME).then(cache => {
+        //   return cache.match(event.request).then(response => {
+        //     return response || fetch(event.request);
+        //   });
+        // })
+    );
+});
+
+
