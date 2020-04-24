943f764627e5b50fc32476c6c4c1c9792f4a2c37
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-04-24 11:37:32 -0600

    working offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,23 +23,7 @@ self.addEventListener("install", function (event) {
     //  self.skipWaiting();
 });
 
-// activate
-// self.addEventListener("activate", function(evt) {
-//   evt.waitUntil(
-//     caches.keys().then(keyList => {
-//       return Promise.all(
-//         keyList.map(key => {
-//           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-//             console.log("Removing old cache data", key);
-//             return caches.delete(key);
-//           }
-//         })
-//       );
-//     })
-//   );
 
-//   self.clients.claim();
-// });
 
 self.addEventListener("fetch", function (event) {
     // cache all get requests to /api routes
@@ -77,11 +61,7 @@ self.addEventListener("fetch", function (event) {
                 }
             });
         })
-        // caches.open(CACHE_NAME).then(cache => {
-        //   return cache.match(event.request).then(response => {
-        //     return response || fetch(event.request);
-        //   });
-        // })
+
     );
 });
 
